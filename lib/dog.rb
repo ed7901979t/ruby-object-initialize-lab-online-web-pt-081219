0fd433397b0e840231d92161499737e4a61a9570
clean-command-1851 <ed7901979t@gmail.com>
2019-08-24 17:10:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,18 @@
-class Dog  
-  def initialize(breed, "Mutt")
-    @breed=breed
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
-  def breed(breed, "Mutt")
-    @breed=breed
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
+
+  def name
+    @name
+  end
   def breed
-    @breed=breed
-  end
-  
-  
-
-
-
-
-
-
-
-
-
-
-end
+    @breed
+  end 
+end 
